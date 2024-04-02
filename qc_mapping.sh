6ce032425f90e4ff3a084c70945fe9efb2208f9c
bridlin <bridlinbarckmann@gmail.com>
2024-04-02 10:07:05 +0200

    removed line command

--- qc_mapping.sh
+++ qc_mapping.sh
@@ -25,26 +25,26 @@ source scripts/finding_ORIs/config_mapping.txt
 mkdir $output_dir
 
 for sample in "${input_list[@]}"; do
-# fastqc $fastq_directory/$sample\L001_R1_001.fastq.gz --outdir $output_dir &&
-# fastqc $fastq_directory/$sample\L001_R2_001.fastq.gz --outdir $output_dir &&
-# cutadapt -u -10 -u 10   -U -10 -U 10  \
-#     -o $fastq_directory/$sample\_R1_5tailtrimmed.fastq.gz  \
-#     -p $fastq_directory/$sample\_R2_5tailtrimmed.fastq.gz  \
-#     $fastq_directory/$sample\L001_R1_001.fastq.gz $fastq_directory/$sample\L001_R2_001.fastq.gz &&
-# cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA  -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
-#     -o $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz \
-#     -p $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz  \
-#     $fastq_directory/$sample\_R1_5tailtrimmed.fastq.gz  $fastq_directory/$sample\_R2_5tailtrimmed.fastq.gz \
-#     --minimum-length 30 > $output_dir/$sample\_cutadapt_report.txt &&
-# trimmomatic PE \
-#     -threads 4 \
-#     -trimlog $output_dir/$sample\trim \
-#     $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz \
-#     $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20_un.fastq.gz \
-#     $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20_un.fastq.gz \
-#     SLIDINGWINDOW:4:20 MINLEN:40 &&
-# fastqc $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz --outdir $output_dir &&
-# fastqc $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz --outdir $output_dir &&
+fastqc $fastq_directory/$sample\L001_R1_001.fastq.gz --outdir $output_dir &&
+fastqc $fastq_directory/$sample\L001_R2_001.fastq.gz --outdir $output_dir &&
+cutadapt -u -10 -u 10   -U -10 -U 10  \
+    -o $fastq_directory/$sample\_R1_5tailtrimmed.fastq.gz  \
+    -p $fastq_directory/$sample\_R2_5tailtrimmed.fastq.gz  \
+    $fastq_directory/$sample\L001_R1_001.fastq.gz $fastq_directory/$sample\L001_R2_001.fastq.gz &&
+cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA  -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
+    -o $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz \
+    -p $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz  \
+    $fastq_directory/$sample\_R1_5tailtrimmed.fastq.gz  $fastq_directory/$sample\_R2_5tailtrimmed.fastq.gz \
+    --minimum-length 30 > $output_dir/$sample\_cutadapt_report.txt &&
+trimmomatic PE \
+    -threads 4 \
+    -trimlog $output_dir/$sample\trim \
+    $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz \
+    $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20_un.fastq.gz \
+    $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20_un.fastq.gz \
+    SLIDINGWINDOW:4:20 MINLEN:40 &&
+fastqc $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz --outdir $output_dir &&
+fastqc $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz --outdir $output_dir &&
 bowtie2 \
     -k1 \
     -x $genome \
