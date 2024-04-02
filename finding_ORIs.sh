6630e7b4da0a53fc4bd291a0e4c7d05d5544a509
bridlin <bridlinbarckmann@gmail.com>
2024-04-02 16:42:43 +0200

    mistake in the syntax of the last for loop and line command

--- finding_ORIs.sh
+++ finding_ORIs.sh
@@ -58,7 +58,7 @@ mkdir $output_dir
 
 #PEAK CALLING ALONE:
 
-for sample in "${input_list[@]}"; do
+#for sample in "${input_list[@]}"; do
 	# macs2 callpeak  \
 	# 	--bdg  -t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
 	# 	-f BAMPE \
@@ -68,69 +68,69 @@ for sample in "${input_list[@]}"; do
 	# 	-s 130 \
 	# 	-m 10 30 \
 	# 	--gsize 2.5e7 &&
-	macs2 callpeak  \
-		--bdg  -t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
-		-f BAMPE \
-		-n $sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005  \
-		--outdir $output_dir/ \
-		-p 5e-2 \
-		-s 130 \
-		-m 10 30 \
-		--gsize 2.5e7 \
-; done
+# 	macs2 callpeak  \
+# 		--bdg  -t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
+# 		-f BAMPE \
+# 		-n $sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005  \
+# 		--outdir $output_dir/ \
+# 		-p 5e-2 \
+# 		-s 130 \
+# 		-m 10 30 \
+# 		--gsize 2.5e7 \
+# ; done
 
 
-#PEAK SELECTION :
-#1) Sorting out overlapping peaks:
+# #PEAK SELECTION :
+# #1) Sorting out overlapping peaks:
 
-for overlap in "${overlap_list[@]}" ; do for sample in "${input_list[@]}" ; do
-	bedtools intersect \
-		-wa  \
-		-e  \
-		-v \
-		-f 0.$overlap  \
-		-F 0.$overlap  \
-		-a $output_dir/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-		-b $output_dir/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-	> $output_dir/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
-	bedtools intersect \
-		-wa  \
-		-e  \
-		-v \
-		-f 0.$overlap  \
-		-F 0.$overlap  \
-		-a $output_dir/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-		-b $output_dir/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-		> $output_dir/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-;done ;done
+# for overlap in "${overlap_list[@]}" ; do for sample in "${input_list[@]}" ; do
+# 	bedtools intersect \
+# 		-wa  \
+# 		-e  \
+# 		-v \
+# 		-f 0.$overlap  \
+# 		-F 0.$overlap  \
+# 		-a $output_dir/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 		-b $output_dir/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 	> $output_dir/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
+# 	bedtools intersect \
+# 		-wa  \
+# 		-e  \
+# 		-v \
+# 		-f 0.$overlap  \
+# 		-F 0.$overlap  \
+# 		-a $output_dir/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 		-b $output_dir/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 		> $output_dir/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# ;done ;done
 
 
-#2) Selecting peaks in a window:
+# #2) Selecting peaks in a window:
 
-for window in "${window_list[@]}"; do for sample in "${input_list[@]}" ; do for overlap in "${overlap_list[@]}" ; do
-	bedtools window \
-		-w $window \
-		-a $output_dir/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-		-b $output_dir/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-		> $output_dir/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  &&
-	cat $output_dir/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| cut -f 1,2,3,4,5,6,7,8,9,10 \
-	| sort -k1,1 -k2,2n  \
-	| uniq \
-	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}' \
-	> $output_dir/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
-	cat $output_dir/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| cut -f 11,12,13,14,15,16,17,18,19,20 \
-	| sort -k1,1 -k2,2n  \
-	| uniq \
-	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}'  \
-	> $output_dir/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
-	cat \
-	$output_dir/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
-	$output_dir/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| sort -k1,1 -k2,2n \
-	> $output_dir/catMinus-Plus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
-;done ;done ;done
+# for window in "${window_list[@]}"; do for sample in "${input_list[@]}" ; do for overlap in "${overlap_list[@]}" ; do
+# 	bedtools window \
+# 		-w $window \
+# 		-a $output_dir/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 		-b $output_dir/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 		> $output_dir/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  &&
+# 	cat $output_dir/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| cut -f 1,2,3,4,5,6,7,8,9,10 \
+# 	| sort -k1,1 -k2,2n  \
+# 	| uniq \
+# 	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}' \
+# 	> $output_dir/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
+# 	cat $output_dir/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| cut -f 11,12,13,14,15,16,17,18,19,20 \
+# 	| sort -k1,1 -k2,2n  \
+# 	| uniq \
+# 	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}'  \
+# 	> $output_dir/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
+# 	cat \
+# 	$output_dir/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
+# 	$output_dir/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| sort -k1,1 -k2,2n \
+# 	> $output_dir/catMinus-Plus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
+# ;done ;done ;done
 
 #ORI SELECTION
 #Ori-selection strand-seperated Minus-Plus direction 
@@ -166,5 +166,5 @@ for window in "${window_list[@]}"; do for sample in "${input_list[@]}" ; do for 
 	| awk '!seen[$1 $3]++' | sort -k3  \
 	| awk '!seen[$1 $2]++' | sort -k1,1 -k2,2n \
 	> $output_dir/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks_withStrand.bed   \
-	awk ' {print  $1 "\t" $2  "\t" $3  "\t" $4  "\t" $5  "\t""." "\t" $7}' $output_dir/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks_withStrand.bed  > $output_dir/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks.bed
+	awk ' {print  $1 "\t" $2  "\t" $3  "\t" $4  "\t" $5  "\t""." "\t" $7}' $output_dir/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks_withStrand.bed  > $output_dir/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks.bed \
 ;done ;done ;done
