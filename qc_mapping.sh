353134e49373a8b648553d66520d630844eaf473
bridlin <bridlinbarckmann@gmail.com>
2024-04-25 09:56:48 +0200

    removed line command

--- qc_mapping.sh
+++ qc_mapping.sh
@@ -28,41 +28,41 @@ mkdir $output_dir/reports
 
 
 for sample in "${input_list[@]}"; do
-# fastqc $fastq_directory/$sample\L001_R1_001.fastq.gz --outdir $output_dir &&
-# fastqc $fastq_directory/$sample\L001_R2_001.fastq.gz --outdir $output_dir &&
-# cutadapt -u -10 -u 10   -U -10 -U 10  \
-#     -o $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  \
-#     -p $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz  \
-#     $fastq_directory/$sample\L001_R1_001.fastq.gz $fastq_directory/$sample\L001_R2_001.fastq.gz &&
-# cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA  -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
-#     -o $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz \
-#     -p $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz  \
-#     $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz \
-#     --minimum-length 30 > $output_dir//reports/$sample\_cutadapt_report.txt &&
-# trimmomatic PE \
-#     -threads 4 \
-#     -trimlog $output_dir/$sample\trim \
-#     $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz \
-#     $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20_un.fastq.gz \
-#     $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20_un.fastq.gz \
-#     SLIDINGWINDOW:4:20 MINLEN:40 &&
-# fastqc $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc &&
-# fastqc $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc &&
-# bowtie2 \
-#     -k1 \
-#     -x $genome \
-#     -1 $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz \
-#     -2 $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz   \
-#     -S $output_dir/$sample\aln-pe_$genome_prefix\.sam \
-#     2> $output_dir/reports/$sample\_bowtie.log &&
-# samtools view -S -b $output_dir/$sample\aln-pe_$genome_prefix\.sam > $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam &&
-# samtools sort $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam -o $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam &&
-# samtools reheader -c 'grep -v ^@PG' $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam  > $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam &&
-# picard CollectInsertSizeMetrics \
-#     -I $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam \
-#     -O $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_metrics.txt \
-#     -H $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_histogram.pdf \
-#     -M 0.5  &&
+fastqc $fastq_directory/$sample\L001_R1_001.fastq.gz --outdir $output_dir &&
+fastqc $fastq_directory/$sample\L001_R2_001.fastq.gz --outdir $output_dir &&
+cutadapt -u -10 -u 10   -U -10 -U 10  \
+    -o $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  \
+    -p $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz  \
+    $fastq_directory/$sample\L001_R1_001.fastq.gz $fastq_directory/$sample\L001_R2_001.fastq.gz &&
+cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA  -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
+    -o $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz \
+    -p $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz  \
+    $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz \
+    --minimum-length 30 > $output_dir//reports/$sample\_cutadapt_report.txt &&
+trimmomatic PE \
+    -threads 4 \
+    -trimlog $output_dir/$sample\trim \
+    $fastq_directory/$sample\L001_R1_001_5-3trimmed.fastq.gz $fastq_directory/$sample\L001_R2_001_5-3trimmed.fastq.gz \
+    $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20_un.fastq.gz \
+    $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20_un.fastq.gz \
+    SLIDINGWINDOW:4:20 MINLEN:40 &&
+fastqc $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc &&
+fastqc $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc &&
+bowtie2 \
+    -k1 \
+    -x $genome \
+    -1 $fastq_directory/$sample\L001_R1_001_5-3trimmed_q20.fastq.gz \
+    -2 $fastq_directory/$sample\L001_R2_001_5-3trimmed_q20.fastq.gz   \
+    -S $output_dir/$sample\aln-pe_$genome_prefix\.sam \
+    2> $output_dir/reports/$sample\_bowtie.log &&
+samtools view -S -b $output_dir/$sample\aln-pe_$genome_prefix\.sam > $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam &&
+samtools sort $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam -o $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam &&
+samtools reheader -c 'grep -v ^@PG' $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam  > $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam &&
+picard CollectInsertSizeMetrics \
+    -I $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam \
+    -O $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_metrics.txt \
+    -H $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_histogram.pdf \
+    -M 0.5  &&
 picard  MarkDuplicates \
     --REMOVE_DUPLICATES true \
     -I $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam \
