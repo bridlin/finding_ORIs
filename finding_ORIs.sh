f42e5cd91cc362658604096d9d45d81778fc9e91
bridlin <bridlinbarckmann@gmail.com>
2024-05-28 15:10:21 +0200

    changed input list to controls

--- finding_ORIs.sh
+++ finding_ORIs.sh
@@ -61,143 +61,143 @@ for sample in "${input_list[@]}"; do
 
 #PEAK CALLING without control (alone): done seperatly for minus and plus strand originating read pairs. Narrow peaks are called with a p-value of 5e-2. The effective genome size is set to 2.5e7 bp for T.brucei.
 
-for sample in "${input_list[@]}"; do
-	if $with_control == true; 
-	then
-		macs2 callpeak  \
-			--bdg  \
-			-t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
-			-c $read_directory/$sample\_F2R1_control_$file_prefix\.bam  \
-			-f BAMPE \
-			-n $sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005   \
-			--outdir $output_dir/peak_calling/ \
-			-p 5e-2 \
-			-s 130 \
-			-m 10 30 \
-			--gsize 2.5e7 &&
-		macs2 callpeak  \
-			--bdg  \
-			-t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
-			-c $read_directory/$sample\_F1R2_control_$file_prefix\.bam  \
-			-f BAMPE \
-			-n $sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005  \
-			--outdir $output_dir/peak_calling/ \
-			-p 5e-2 \
-			-s 130 \
-			-m 10 30 \
-			--gsize 2.5e7 \
+# for sample in "${input_list[@]}"; do
+# 	if $with_control == true; 
+# 	then
+# 		macs2 callpeak  \
+# 			--bdg  \
+# 			-t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
+# 			-c $read_directory/$sample\_F2R1_control_$file_prefix\.bam  \
+# 			-f BAMPE \
+# 			-n $sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005   \
+# 			--outdir $output_dir/peak_calling/ \
+# 			-p 5e-2 \
+# 			-s 130 \
+# 			-m 10 30 \
+# 			--gsize 2.5e7 &&
+# 		macs2 callpeak  \
+# 			--bdg  \
+# 			-t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
+# 			-c $read_directory/$sample\_F1R2_control_$file_prefix\.bam  \
+# 			-f BAMPE \
+# 			-n $sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005  \
+# 			--outdir $output_dir/peak_calling/ \
+# 			-p 5e-2 \
+# 			-s 130 \
+# 			-m 10 30 \
+# 			--gsize 2.5e7 \
 		
-	else
-		macs2 callpeak  \
-			--bdg  \
-			-t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
-			-f BAMPE \
-			-n $sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005   \
-			--outdir $output_dir/peak_calling/ \
-			-p 5e-2 \
-			-s 130 \
-			-m 10 30 \
-			--gsize 2.5e7 &&
-		macs2 callpeak  \
-			--bdg  \
-			-t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
-			-f BAMPE \
-			-n $sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005  \
-			--outdir $output_dir/peak_calling/ \
-			-p 5e-2 \
-			-s 130 \
-			-m 10 30 \
-			--gsize 2.5e7 \
-	fi
-; done
+# 	else
+# 		macs2 callpeak  \
+# 			--bdg  \
+# 			-t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
+# 			-f BAMPE \
+# 			-n $sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005   \
+# 			--outdir $output_dir/peak_calling/ \
+# 			-p 5e-2 \
+# 			-s 130 \
+# 			-m 10 30 \
+# 			--gsize 2.5e7 &&
+# 		macs2 callpeak  \
+# 			--bdg  \
+# 			-t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
+# 			-f BAMPE \
+# 			-n $sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005  \
+# 			--outdir $output_dir/peak_calling/ \
+# 			-p 5e-2 \
+# 			-s 130 \
+# 			-m 10 30 \
+# 			--gsize 2.5e7 \
+# 	fi
+# ; done
 
 
-#PEAK FILTERING: 
-#1) Sorting out overlapping peaks: we used 50% of maximal overlap for at least one peak for the selection of non-overlapping peaks. 
+# #PEAK FILTERING: 
+# #1) Sorting out overlapping peaks: we used 50% of maximal overlap for at least one peak for the selection of non-overlapping peaks. 
 
-for overlap in "${overlap_list[@]}" ; do for sample in "${input_list[@]}" ; do
-	bedtools intersect \
-		-wa  \
-		-e  \
-		-v \
-		-f 0.$overlap  \
-		-F 0.$overlap  \
-		-a $output_dir/peak_calling/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-		-b $output_dir/peak_calling/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-	> $output_dir/peak_filtering/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
-	bedtools intersect \
-		-wa  \
-		-e  \
-		-v \
-		-f 0.$overlap  \
-		-F 0.$overlap  \
-		-a $output_dir/peak_calling/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-		-b $output_dir/peak_calling/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
-		> $output_dir/peak_filtering/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-;done ;done
+# for overlap in "${overlap_list[@]}" ; do for sample in "${input_list[@]}" ; do
+# 	bedtools intersect \
+# 		-wa  \
+# 		-e  \
+# 		-v \
+# 		-f 0.$overlap  \
+# 		-F 0.$overlap  \
+# 		-a $output_dir/peak_calling/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 		-b $output_dir/peak_calling/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 	> $output_dir/peak_filtering/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
+# 	bedtools intersect \
+# 		-wa  \
+# 		-e  \
+# 		-v \
+# 		-f 0.$overlap  \
+# 		-F 0.$overlap  \
+# 		-a $output_dir/peak_calling/$sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 		-b $output_dir/peak_calling/$sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p005_peaks.narrowPeak \
+# 		> $output_dir/peak_filtering/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# ;done ;done
 
 
-#2) Selecting peaks in a selected window: we used 500 bp max distance. 
+# #2) Selecting peaks in a selected window: we used 500 bp max distance. 
 
-for window in "${window_list[@]}"; do for sample in "${input_list[@]}" ; do for overlap in "${overlap_list[@]}" ; do
-	bedtools window \
-		-w $window \
-		-a $output_dir/peak_filtering/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-		-b $output_dir/peak_filtering/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-		> $output_dir/peak_filtering/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  &&
-	# seperating the bedtools window output in Minus and Plus peaks tables
-	cat $output_dir/peak_filtering/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| cut -f 1,2,3,4,5,6,7,8,9,10 \
-	| sort -k1,1 -k2,2n  \
-	| uniq \
-	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}' \
-	> $output_dir/peak_filtering/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
-	cat $output_dir/peak_filtering/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| cut -f 11,12,13,14,15,16,17,18,19,20 \
-	| sort -k1,1 -k2,2n  \
-	| uniq \
-	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}'  \
-	> $output_dir/peak_filtering/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-;done ;done ;done
+# for window in "${window_list[@]}"; do for sample in "${input_list[@]}" ; do for overlap in "${overlap_list[@]}" ; do
+# 	bedtools window \
+# 		-w $window \
+# 		-a $output_dir/peak_filtering/$sample\-alone_Minus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 		-b $output_dir/peak_filtering/$sample\-alone_Plus_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 		> $output_dir/peak_filtering/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  &&
+# 	# seperating the bedtools window output in Minus and Plus peaks tables
+# 	cat $output_dir/peak_filtering/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| cut -f 1,2,3,4,5,6,7,8,9,10 \
+# 	| sort -k1,1 -k2,2n  \
+# 	| uniq \
+# 	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}' \
+# 	> $output_dir/peak_filtering/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
+# 	cat $output_dir/peak_filtering/union$window\_$sample\-alone_Minus-Plus-nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| cut -f 11,12,13,14,15,16,17,18,19,20 \
+# 	| sort -k1,1 -k2,2n  \
+# 	| uniq \
+# 	| awk 'BEGIN { OFS="\t" } {if ($4 ~ /_Minus_/) print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "-" "\t" $7 "\t"$8 "\t" $9 "\t" $10 ;else print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" "+" "\t" $7 "\t"$8 "\t" $9 "\t" $10}'  \
+# 	> $output_dir/peak_filtering/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# ;done ;done ;done
 
-#3)last filtering step : selecting peak pairs in minus upstream plus downstram direction
-#ORI SELECTION  defining origina as the region between filtered peak pairs
+# #3)last filtering step : selecting peak pairs in minus upstream plus downstram direction
+# #ORI SELECTION  defining origina as the region between filtered peak pairs
 
-#  (a) closest iu: closest peak downstream  to the minus strand peaks, awk line to exclude all distances bigger than the window size
+# #  (a) closest iu: closest peak downstream  to the minus strand peaks, awk line to exclude all distances bigger than the window size
 
-for window in "${window_list[@]}"; do for sample in "${input_list[@]}" ; do for overlap in "${overlap_list[@]}" ; do
-	bedtools closest \
-		-iu \
-		-D ref \
-		-t all \
-		-a $output_dir/peak_filtering/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
-		-b $output_dir/peak_filtering/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| awk -v dist="$window"  'BEGIN { OFS="\t" } {if ($12 != -1 && $21 < dist ) {print $0} }'\
-	> $output_dir/peak_filtering/closest-iu_Minus-Plus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
-# (b) closest id: closest peak upstream to the plus strand peaks, awk line to exclude all distances bigger than the window size	
-	bedtools closest \
-		-id \
-		-D ref \
-		-t all \
-		-a $output_dir/peak_filtering/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-		-b $output_dir/peak_filtering/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
-	| awk -v dist="$window"  'BEGIN { OFS="\t" } {if ($12 != -1 && $21 > -dist ) {print $0} }'  \
-	> $output_dir/peak_filtering/closest-id_Plus-Minus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
-# (c) selection of the origins as the region between a minus downstream and a following plus peaks from the closest output table:
-# to be considered: the closest iu option is not ignoring upstream peaks that are overlapping. Those are filtered out by distance ($21) smaller equal 0 and start coordinate of a ($2) bigger than start coordinate of b ($12).
-	cat $output_dir/peak_filtering/closest-iu_Minus-Plus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| awk 'BEGIN { OFS="\t" } {if ($21 > 0 ) {print $1 "\t" $3 "\t" $12 "\t" $4 "\t" "." "\t" $6 "\t" $21}   else if ($21 == 0 && $2 < $12 ) {print $1 "\t" $12 "\t" $3 "\t" $4 "\t" "." "\t" $6 "\t" $21} }' \
-	> $output_dir/peak_filtering/Minus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed &&
-# (d) selection of the Origins as the region between a plus upstream and a following Minus peaks from the closest output table:
-# to be considered: the closest id option is not ignoring downstream peaks that are overlapping. Those are filtered out by distance ($21) equal  0 and start coordinate of a ($2) smaller than start coordinate of b ($12).	
-	cat $output_dir/peak_filtering/closest-id_Plus-Minus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
-	| awk 'BEGIN { OFS="\t" } {if ($21 < 0 ) {print $1 "\t" $13 "\t" $2 "\t" $4 "\t" "." "\t" "-" "\t" $21}  else if ($21 == 0 && $2 > $12 ) {print $1 "\t" $2 "\t" $13 "\t" $4 "\t" "." "\t" "-" "\t" $21} }' \
-	> $output_dir/peak_filtering/Plus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed &&
-# (e) union of the two Ori tables, most of the Ori regions are identical, so the union is done by sorting the regions by start coordinate and removing duplicates and region with tha same sart or end coordinate keeping the smaller one
-	echo $output_dir/peak_filtering/Plus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed && \
-	echo $output_dir/peak_filtering/Minus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed && \
-	cat $output_dir/peak_filtering/Plus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed $output_dir/peak_filtering/Minus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed| sort -k1,1 -k2,2n | awk '!a[$1 $2 $3]++' | sort -rk2 | awk '!seen[$1 $3]++' | sort -k3 | awk '!seen[$1 $2]++' | sort -k1,1 -k2,2n> $output_dir/oris/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks_withStrand.bed &&  
-	cat $output_dir/oris/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks_withStrand.bed | \
-	awk ' {print  $1 "\t" $2  "\t" $3  "\t" $4  "\t" $5  "\t" "." "\t" $7}'  \
-	> $output_dir/oris/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks.bed \
-;done ;done ;done
+# for window in "${window_list[@]}"; do for sample in "${input_list[@]}" ; do for overlap in "${overlap_list[@]}" ; do
+# 	bedtools closest \
+# 		-iu \
+# 		-D ref \
+# 		-t all \
+# 		-a $output_dir/peak_filtering/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
+# 		-b $output_dir/peak_filtering/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| awk -v dist="$window"  'BEGIN { OFS="\t" } {if ($12 != -1 && $21 < dist ) {print $0} }'\
+# 	> $output_dir/peak_filtering/closest-iu_Minus-Plus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
+# # (b) closest id: closest peak upstream to the plus strand peaks, awk line to exclude all distances bigger than the window size	
+# 	bedtools closest \
+# 		-id \
+# 		-D ref \
+# 		-t all \
+# 		-a $output_dir/peak_filtering/Plus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 		-b $output_dir/peak_filtering/Minus-union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak  \
+# 	| awk -v dist="$window"  'BEGIN { OFS="\t" } {if ($12 != -1 && $21 > -dist ) {print $0} }'  \
+# 	> $output_dir/peak_filtering/closest-id_Plus-Minus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak &&
+# # (c) selection of the origins as the region between a minus downstream and a following plus peaks from the closest output table:
+# # to be considered: the closest iu option is not ignoring upstream peaks that are overlapping. Those are filtered out by distance ($21) smaller equal 0 and start coordinate of a ($2) bigger than start coordinate of b ($12).
+# 	cat $output_dir/peak_filtering/closest-iu_Minus-Plus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| awk 'BEGIN { OFS="\t" } {if ($21 > 0 ) {print $1 "\t" $3 "\t" $12 "\t" $4 "\t" "." "\t" $6 "\t" $21}   else if ($21 == 0 && $2 < $12 ) {print $1 "\t" $12 "\t" $3 "\t" $4 "\t" "." "\t" $6 "\t" $21} }' \
+# 	> $output_dir/peak_filtering/Minus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed &&
+# # (d) selection of the Origins as the region between a plus upstream and a following Minus peaks from the closest output table:
+# # to be considered: the closest id option is not ignoring downstream peaks that are overlapping. Those are filtered out by distance ($21) equal  0 and start coordinate of a ($2) smaller than start coordinate of b ($12).	
+# 	cat $output_dir/peak_filtering/closest-id_Plus-Minus_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.narrowPeak \
+# 	| awk 'BEGIN { OFS="\t" } {if ($21 < 0 ) {print $1 "\t" $13 "\t" $2 "\t" $4 "\t" "." "\t" "-" "\t" $21}  else if ($21 == 0 && $2 > $12 ) {print $1 "\t" $2 "\t" $13 "\t" $4 "\t" "." "\t" "-" "\t" $21} }' \
+# 	> $output_dir/peak_filtering/Plus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed &&
+# # (e) union of the two Ori tables, most of the Ori regions are identical, so the union is done by sorting the regions by start coordinate and removing duplicates and region with tha same sart or end coordinate keeping the smaller one
+# 	echo $output_dir/peak_filtering/Plus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed && \
+# 	echo $output_dir/peak_filtering/Minus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed && \
+# 	cat $output_dir/peak_filtering/Plus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed $output_dir/peak_filtering/Minus_oris_union$window\_$sample\-alone_nonoverlap$overlap\_narrow_p005_peaks.bed| sort -k1,1 -k2,2n | awk '!a[$1 $2 $3]++' | sort -rk2 | awk '!seen[$1 $3]++' | sort -k3 | awk '!seen[$1 $2]++' | sort -k1,1 -k2,2n> $output_dir/oris/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks_withStrand.bed &&  
+# 	cat $output_dir/oris/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks_withStrand.bed | \
+# 	awk ' {print  $1 "\t" $2  "\t" $3  "\t" $4  "\t" $5  "\t" "." "\t" $7}'  \
+# 	> $output_dir/oris/ORI_$sample\-alone_union$window\_nonoverlap$overlap\_narrow_p005_peaks.bed \
+# ;done ;done ;done
--- qc_mapping.sh
+++ qc_mapping.sh
@@ -28,50 +28,50 @@ mkdir $output_dir/reports
 
 
 for sample in "${input_list[@]}"; do
-fastqc $fastq_directory/$sample$readname_postfix_R1 --outdir $output_dir &&
-fastqc $fastq_directory/$sample$readname_postfix_R2 --outdir $output_dir &&
-cutadapt -u -10 -u 10   -U -10 -U 10  \
-    -o $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  \
-    -p $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz  \
-    $fastq_directory/$sample$readname_postfix_R1 $fastq_directory/$sample$readname_postfix_R2 &&
-cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA  -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
-    -o $fastq_directory/$sample\R1_5-3trimmed.fastq.gz \
-    -p $fastq_directory/$sample\R2_5-3trimmed.fastq.gz  \
-    $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz \
-    --minimum-length 30 > $output_dir//reports/$sample\_cutadapt_report.txt &&
-trimmomatic PE \
-    -threads 4 \
-    -trimlog $output_dir/$sample\trim \
-    $fastq_directory/$sample\R1_5-3trimmed.fastq.gz $fastq_directory/$sample\R2_5-3trimmed.fastq.gz \
-    $fastq_directory/$sample\R1_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\R1_5-3trimmed_q20_un.fastq.gz \
-    $fastq_directory/$sample\R2_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\R2_5-3trimmed_q20_un.fastq.gz \
-    SLIDINGWINDOW:4:20 MINLEN:40 &&
-fastqc $fastq_directory/$sample\R1_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc &&
-fastqc $fastq_directory/$sample\R2_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc ;done 
-# bowtie2 \
-#     -k1 \
-#     -x $genome \
-#     -1 $fastq_directory/$sample\R1_5-3trimmed_q20.fastq.gz \
-#     -2 $fastq_directory/$sample\R2_5-3trimmed_q20.fastq.gz   \
-#     -S $output_dir/$sample\aln-pe_$genome_prefix\.sam \
-#     2> $output_dir/reports/$sample\_bowtie.log &&
-# samtools view -S -b $output_dir/$sample\aln-pe_$genome_prefix\.sam > $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam &&
-# samtools sort $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam -o $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam &&
-# samtools reheader -c 'grep -v ^@PG' $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam  > $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam &&
-# picard CollectInsertSizeMetrics \
-#     -I $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam \
-#     -O $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_metrics.txt \
-#     -H $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_histogram.pdf \
-#     -M 0.5  &&
-# picard  MarkDuplicates \
-#     --REMOVE_DUPLICATES true \
-#     -I $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam \
-#     -O $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered_dups-removed.bam \
-#     -M $output_dir/reports/$sample\aln-pe_$genome_prefix\_marked_dup_metrics.txt &&
-# samtools index $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered_dups-removed.bam &&
-# rm -f  $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam &&
-# rm -f  $output_dir/$sample\aln-pe_$genome_prefix\.sam &&
-# rm -f  $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam ;done
+# fastqc $fastq_directory/$sample$readname_postfix_R1 --outdir $output_dir &&
+# fastqc $fastq_directory/$sample$readname_postfix_R2 --outdir $output_dir &&
+# cutadapt -u -10 -u 10   -U -10 -U 10  \
+#     -o $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  \
+#     -p $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz  \
+#     $fastq_directory/$sample$readname_postfix_R1 $fastq_directory/$sample$readname_postfix_R2 &&
+# cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA  -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT  \
+#     -o $fastq_directory/$sample\R1_5-3trimmed.fastq.gz \
+#     -p $fastq_directory/$sample\R2_5-3trimmed.fastq.gz  \
+#     $fastq_directory/$sample\R1_5tailtrimmed.fastq.gz  $fastq_directory/$sample\R2_5tailtrimmed.fastq.gz \
+#     --minimum-length 30 > $output_dir//reports/$sample\_cutadapt_report.txt &&
+# trimmomatic PE \
+#     -threads 4 \
+#     -trimlog $output_dir/$sample\trim \
+#     $fastq_directory/$sample\R1_5-3trimmed.fastq.gz $fastq_directory/$sample\R2_5-3trimmed.fastq.gz \
+#     $fastq_directory/$sample\R1_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\R1_5-3trimmed_q20_un.fastq.gz \
+#     $fastq_directory/$sample\R2_5-3trimmed_q20.fastq.gz   $fastq_directory/$sample\R2_5-3trimmed_q20_un.fastq.gz \
+#     SLIDINGWINDOW:4:20 MINLEN:40 &&
+# fastqc $fastq_directory/$sample\R1_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc &&
+# fastqc $fastq_directory/$sample\R2_5-3trimmed_q20.fastq.gz --outdir $output_dir/fastqc && 
+bowtie2 \
+    -k1 \
+    -x $genome \
+    -1 $fastq_directory/$sample\R1_5-3trimmed_q20.fastq.gz \
+    -2 $fastq_directory/$sample\R2_5-3trimmed_q20.fastq.gz   \
+    -S $output_dir/$sample\aln-pe_$genome_prefix\.sam \
+    2> $output_dir/reports/$sample\_bowtie.log &&
+samtools view -S -b $output_dir/$sample\aln-pe_$genome_prefix\.sam > $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam &&
+samtools sort $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam -o $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam &&
+samtools reheader -c 'grep -v ^@PG' $output_dir/$sample\aln-pe_$genome_prefix\_sorted.bam  > $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam &&
+picard CollectInsertSizeMetrics \
+    -I $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam \
+    -O $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_metrics.txt \
+    -H $output_dir/reports/$sample\aln-pe_$genome_prefix\_sorted_reheadered_insert_size_histogram.pdf \
+    -M 0.5  &&
+picard  MarkDuplicates \
+    --REMOVE_DUPLICATES true \
+    -I $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam \
+    -O $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered_dups-removed.bam \
+    -M $output_dir/reports/$sample\aln-pe_$genome_prefix\_marked_dup_metrics.txt &&
+samtools index $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered_dups-removed.bam &&
+rm -f  $output_dir/$sample\aln-pe_$genome_prefix\_sorted_reheadered.bam &&
+rm -f  $output_dir/$sample\aln-pe_$genome_prefix\.sam &&
+rm -f  $output_dir/$sample\aln-pe_$genome_prefix\.sam.bam ;done
 
 multiqc   \
     $output_dir \
