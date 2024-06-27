838feb283569220d1bc25ecf74950f5cdb5a4de4
bridlin <bridlinbarckmann@gmail.com>
2024-06-27 11:08:14 +0200

    testing --llocal parameter macs

--- finding_ORIs.sh
+++ finding_ORIs.sh
@@ -67,29 +67,32 @@ for sample in "${input_list[@]}"; do
 	macs2 callpeak  \
 		--bdg  \
 		-t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
+		-c $read_directory/$sample\_control_F2R1_$file_prefix\.bam  \
 		-f BAMPE \
 		-n $sample\-alone_Minus_bowtie2_trimmed_uniq_dupsre_narrow_p01   \
 		--outdir $output_dir/peak_calling/ \
-		-p 5e-2 \
 		-s 130 \
+		-p 5e-2 \
 		-m 10 30 \
-		--gsize 2.5e7 \
-		--nolambda &&
+		--llocal \
+		--gsize 2.5e7 &&
 	macs2 callpeak  \
 		--bdg  \
 		-t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
+		-c $read_directory/$sample\_control_F1R2_$file_prefix\.bam  \
 		-f BAMPE \
 		-n $sample\-alone_Plus_bowtie2_trimmed_uniq_dupsre_narrow_p01  \
 		--outdir $output_dir/peak_calling/ \
-		-p 5e-2 \
 		-s 130 \
+		-p 5e-2 \
 		-m 10 30 \
+		--llocal \
 		--gsize 2.5e7 \
-		--nolambda \
 ;done
 
-#-c $read_directory/$sample\_control_F2R1_$file_prefix\.bam  \
-#-c $read_directory/$sample\_control_F1R2_$file_prefix\.bam  \
+#--nolambda \
+#--nolambda \
+#-s size of the reads
 ###PEAK FILTERING: 
 ###1) Sorting out overlapping peaks: we used 50% of maximal overlap for at least one peak for the selection of non-overlapping peaks. 
 
