8cb4a27b73ff3645a3e02413e4216f2cb1fefae6
bridlin <bridlinbarckmann@gmail.com>
2024-08-26 13:27:40 +0200

    removed slocal and llocal parameter form MACS

--- finding_ORIs.sh
+++ finding_ORIs.sh
@@ -74,8 +74,8 @@ for sample in "${input_list[@]}"; do
 		-s 130 \
 		-p 5e-2 \
 		-m 10 30 \
-		--slocal 50000\
-		--llocal 100000\
+		#--slocal 50000\
+		#--llocal 100000\
 		--gsize 2.5e7 &&
 	macs2 callpeak  \
 		--bdg  \
@@ -87,8 +87,8 @@ for sample in "${input_list[@]}"; do
 		-s 130 \
 		-p 5e-2 \
 		-m 10 30 \
-		--slocal 50000\
-		--llocal 100000\
+		#--slocal 50000\
+		#--llocal 100000\
 		--gsize 2.5e7 \
 ;done
 
