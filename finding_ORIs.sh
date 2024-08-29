a133816ec0e8b64f26d19dbc7d57350337fc0a25
bridlin <bridlinbarckmann@gmail.com>
2024-08-29 14:44:47 +0200

    config and line command

--- finding_ORIs.sh
+++ finding_ORIs.sh
@@ -64,19 +64,19 @@ mkdir $output_dir/oris
 
 
 for sample in "${input_list[@]}"; do
-	macs2 callpeak  \
-		--bdg  \
-		-t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
-		-c $read_directory/$sample\_control_F2R1_$file_prefix\.bam  \
-		-f BAMPE \
-		-n $sample\-RNASE_Minus_bowtie2_trimmed_uniq_dupsre_narrow   \
-		--outdir $output_dir/peak_calling/ \
-		-s 130 \
-		-p 5e-2 \
-		-m 10 30 \
-		#--slocal 50000\
-		#--llocal 100000\
-		--gsize 2.5e7 &&
+	# macs2 callpeak  \
+	# 	--bdg  \
+	# 	-t $read_directory/$sample\_F2R1_$file_prefix\.bam   \
+	# 	-c $read_directory/$sample\_control_F2R1_$file_prefix\.bam  \
+	# 	-f BAMPE \
+	# 	-n $sample\-RNASE_Minus_bowtie2_trimmed_uniq_dupsre_narrow   \
+	# 	--outdir $output_dir/peak_calling/ \
+	# 	-s 130 \
+	# 	-p 5e-2 \
+	# 	-m 10 30 \
+	# 	#--slocal 50000\
+	# 	#--llocal 100000\
+	# 	--gsize 2.5e7 &&
 	macs2 callpeak  \
 		--bdg  \
 		-t $read_directory/$sample\_F1R2_$file_prefix\.bam  \
